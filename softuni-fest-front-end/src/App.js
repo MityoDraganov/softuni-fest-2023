afda939c5b2fe542adb56a07b0b2fe38f8b230fc
mitkoprobul <106108077+MityoDraganov@users.noreply.github.com>
2023-10-27 15:01:27 +0300

    routes

--- softuni-fest-front-end/src/App.js
+++ softuni-fest-front-end/src/App.js
@@ -1,11 +1,23 @@
 
 import './App.css';
+import {Routes, Route, } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-        
-    </div>
+    <>
+      <Routes>
+
+        //user routes
+        <Route path="/users/register" element={} />
+        <Route path="/users/login" element={} />
+
+        //bussiness routes
+        <Route path="/bussiness/login" element={} />
+        <Route path="/bussiness/register" element={} />
+
+        <Route path="/bussiness/products" element={} />
+      </Routes>
+    </>
   );
 }
 
