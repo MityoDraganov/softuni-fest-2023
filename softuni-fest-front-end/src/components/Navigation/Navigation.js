cdef94ae046ec521a871149a48c06b716fece0eb
mitkoprobul <106108077+MityoDraganov@users.noreply.github.com>
2023-10-27 16:37:47 +0300

    Update Navigation.js

--- softuni-fest-front-end/src/components/Navigation/Navigation.js
+++ softuni-fest-front-end/src/components/Navigation/Navigation.js
@@ -1,10 +1,15 @@
-import { NavLink } from "react-router-dom"
-import styles from "./Navigation.module.css"
-export const Navigation = () => {
+import { Link, useLocation } from "react-router-dom";
+import styles from "./Navigation.module.css";
 
-    // business nav
+export const Navigation = () => {
+    const { pathname } = useLocation();
+
+    const isActive = (path) => {
+        return path === pathname ? styles.active : "";
+    };
+
     return (
-        <nav className={styles["container"]}>
+        <nav className={styles.container}>
             <ul>
                 <li className={isActive("/business/register")}>
                     <Link to="/business/register">Register</Link>
@@ -16,6 +21,12 @@ export const Navigation = () => {
                     <Link to="/business/products">Products</Link>
                 </li>
             </ul>
+
+            <div>
+                <div className={styles["pfp-container"]}>
+                    <img className={styles["pfp"]} src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/2048px-Default_pfp.svg.png" />
+                </div>
+            </div>
         </nav>
-    )
-}
+    );
+};
