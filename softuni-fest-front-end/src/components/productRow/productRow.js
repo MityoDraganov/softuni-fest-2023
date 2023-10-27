3a70777618db82692c914e0b51a14d28f0038c6e
dingi7 <kamenkanev88@gmail.com>
2023-10-27 19:38:28 +0300

    Css changes

--- softuni-fest-front-end/src/components/productRow/productRow.js
+++ softuni-fest-front-end/src/components/productRow/productRow.js
@@ -1,10 +1,8 @@
-
+import './productRow.css';
 
 export const productRow = (product) => {
-
-
     return(
-        <tr>
+        <tr className="product-row">
             <td>{product.name}</td>
             <td>{product.description}</td>
             <td>{product.price}</td>
