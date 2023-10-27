e07c7def2569694fc57f9652e2c1b80eb1b62b31
mitkoprobul <106108077+MityoDraganov@users.noreply.github.com>
2023-10-27 22:43:39 +0300

    Update index.js

--- softuni-fest-front-end/src/index.js
+++ softuni-fest-front-end/src/index.js
@@ -12,9 +12,9 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <AuthProvider>
-                <ToastContainer>
+
                     <App />
-                </ToastContainer>
+
             </AuthProvider>
         </BrowserRouter>
 
