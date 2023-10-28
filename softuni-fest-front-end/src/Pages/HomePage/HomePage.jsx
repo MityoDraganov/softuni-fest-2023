476497f9945086fe3e6f2512e5ec6efcce2e431b
StefanTodorov32 <bgtrak@gmail.com>
2023-10-29 02:13:27 +0300

    commit

--- softuni-fest-front-end/src/Pages/HomePage/HomePage.jsx
+++ softuni-fest-front-end/src/Pages/HomePage/HomePage.jsx
@@ -1,34 +1,39 @@
 // Home.js
 
-import{ useContext } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
 import styles from './HomePage.module.css';
 
 export default function Home() {
 
-const {accessData} = useContext(AuthContext)
+    const { accessData } = useContext(AuthContext)
 
     return (
         <main className={styles.main}>
             <header className={styles.header}>
                 <h1>Payment Service</h1>
             </header>
-            
+
             <section className={styles.section}>
                 <h2>Welcome to SoftUni Software Fest 2023!</h2>
                 <p>Discover the easiest way to manage your payments.</p>
 
                 <div className={styles.buttons}>
                     {!accessData.accessToken &&
-                    <Link to="/users/register" className={styles.link}>
-                        <button className={styles.button}>Sign Up</button>
-                    </Link>
+                        <Link to="/users/register" className={styles.link}>
+                            <button className={styles.button}>Sign Up</button>
+                        </Link>
                     }
                     <Link to="/learn-more" className={styles.link}>
                         <button className={styles.button}>Learn More</button>
                     </Link>
                 </div>
+                <img
+                    src={`https://bcas.co.za/wp-content/uploads/2015/12/BCEA-PAyments.jpg`}
+                    alt="People paying online"
+                    className={styles.headerImage}
+                />
             </section>
         </main>
     );
