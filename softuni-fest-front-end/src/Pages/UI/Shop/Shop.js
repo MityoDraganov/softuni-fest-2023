fd45c7df9d765c4a1b9386f5ea42c0ea81407f34
mitkoprobul <106108077+MityoDraganov@users.noreply.github.com>
2023-10-28 11:23:37 +0300

    product

--- softuni-fest-front-end/src/Pages/UI/Shop/Shop.js
+++ softuni-fest-front-end/src/Pages/UI/Shop/Shop.js
@@ -2,8 +2,11 @@
 import styles from "./Shop.module.css"
 
 import { Search } from "../../../components/Search/Search"
-import { getAllProducts } from "../../../services/requests"
+import { getAllProducts, getProductsByBusinessId } from "../../../services/requests"
 import { useEffect, useState } from "react"
+
+import { Product } from "../../../components/Product/Product"
+
 
 export const Shop = () => {
 
@@ -14,9 +17,14 @@ export const Shop = () => {
         setSearchValue(e.target.value);
     };
 
-    const getProducts = async () =>{
+    const getProducts = async () => {
         const data = await getAllProducts()
         console.log(data);
+        setProducts(data)
+    }
+
+    const getBussiness = async (bussinessId) => {
+        const data = await getProductsByBusinessId(bussinessId)
         setProducts(data)
     }
 
@@ -28,22 +36,38 @@ export const Shop = () => {
         return product.name.toLowerCase().includes(searchValue.toLowerCase());
     });
 
-    return(
+
+
+    return (
         <div className={styles["container"]}>
-            <h1>Shop rendered</h1>
+
             <div className={styles["search-container"]}>
+                <h2>Search</h2>
 
+                <Search
+                    onInputChange={onChangeHandler}
+                    searchValue={searchValue}
+                />
 
-            <Search 
-                onInputChange={onChangeHandler}
-                searchValue={searchValue}
-            />
-            
-            <ul>
-                {searchValue && filteredProducts.map((product, index) => (
-                    <li key={index}>{product.name}</li> 
+                <ul>
+                    {searchValue && filteredProducts.map((product, index) => (
+                        <li
+                            key={index}
+                            onClick={() => getBussiness(product.owner)}
+                        >
+                            {product.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className={styles["products-container"]}>
+                {filteredProducts.map((product) => (
+                    <Product
+                        key={product._id}
+                        product={product}
+                    />
                 ))}
-            </ul>
             </div>
 
         </div>
