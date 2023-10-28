87fc32dea9032ddba850190cb9d063e5b2a140b0
mitkoprobul <106108077+MityoDraganov@users.noreply.github.com>
2023-10-28 14:12:39 +0300

    THIS CAN CONFLICT !!!

--- softuni-fest-front-end/src/components/ShopModal/ShopModal.js
+++ softuni-fest-front-end/src/components/ShopModal/ShopModal.js
@@ -1,7 +1,10 @@
 import { coinbaseCheckout, stripeCheckout } from "../../services/requests"
 import styles from "./ShopModal.module.css"
 
-export const ShopModal = ({product}) => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+export const ShopModal = ({ product, closeModal }) => {
 
     const handleStripePayment = async () => {
         const response = await stripeCheckout(product._id)
@@ -15,8 +18,9 @@ export const ShopModal = ({product}) => {
     }
 
 
-    return(
+    return (
         <div className={styles["container"]}>
+<<<<<<< Updated upstream
             <h1>{product.name}</h1>
             <h2>{product.owner.companyName}</h2>
             <p>{product.price}</p>
@@ -25,6 +29,36 @@ export const ShopModal = ({product}) => {
             <div className={styles["modal-actions"]}>
                 <button onClick={handleCoinbasePayment}>Pay with crypto</button>
                 <button onClick={handleStripePayment}>Pay with Stripe</button>
+=======
+
+
+            <div>
+                <h2>{product.name}</h2>
+
+                <p>{product.owner.companyName}</p>
+
+                <label>Item description:</label>
+                <p>{product.description}</p>
+
+
+
+                <label>Price:</label>
+                <p>${product.price.toFixed(2)}</p>
+
+                <div className={styles["modal-actions"]}>
+                    <button>Pay with crypto</button>
+                    <button>Pay with Stripe</button>
+                </div>
+
+            </div>
+
+            <div className={styles["modal-close-container"]}>
+
+                <button onClick={closeModal}>
+                    <FontAwesomeIcon icon={faTimes} />
+                </button>
+
+>>>>>>> Stashed changes
             </div>
         </div>
     )
