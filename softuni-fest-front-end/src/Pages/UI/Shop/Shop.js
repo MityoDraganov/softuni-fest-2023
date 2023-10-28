6f4ab9c36a74244807d8b6948cee14eac23b1be4
mitkoprobul <106108077+MityoDraganov@users.noreply.github.com>
2023-10-28 10:22:12 +0300

    search/shop/product init

--- softuni-fest-front-end/src/Pages/UI/Shop/Shop.js
+++ softuni-fest-front-end/src/Pages/UI/Shop/Shop.js
@@ -1,9 +1,43 @@
 
+import styles from "./Shop.module.css"
+
+import { Search } from "../../../components/Search/Search"
+import { getAllProducts } from "../../../services/requests"
+import { useEffect, useState } from "react"
+
 export const Shop = () => {
 
+    const [searchValue, setSearchValue] = useState("")
+    const [products, setProducts] = useState([])
+
+    const onChangeHandler = (e) => {
+        setSearchValue(e.target.value);
+    };
+
+    const getProducts = async () =>{
+        const data = await getAllProducts()
+        console.log(data);
+        setProducts(data)
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, [])
 
     return(
+        <div>
+            <h1>Shop rendered</h1>
+            <Search 
+                onInputChange={onChangeHandler}
+                searchValue={searchValue}
+            />
+            
+            <ul>
+                {products.map((product, index) => (
+                    <li key={index}>{product.name}</li> 
+                ))}
+            </ul>
 
-        <h1>Shop rendered</h1>
+        </div>
     )
 }
