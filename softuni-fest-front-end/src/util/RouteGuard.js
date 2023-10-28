029b683dc0c300acf2bd0bfcdfe16264513b800e
StefanTodorov32 <bgtrak@gmail.com>
2023-10-28 11:34:59 +0300

    commit

--- softuni-fest-front-end/src/util/RouteGuard.js
+++ softuni-fest-front-end/src/util/RouteGuard.js
@@ -1,20 +1,20 @@
 import { useContext } from "react";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { AuthContext } from "../contexts/AuthContext";
 import { errorNotification } from "./notificationHandler";
 
 
 
 export function RouteGuard({ path, component: Component, ...props }) {
-    const { isAuthenticated } = useContext(AuthContext);
+    const { isAuth } = useContext(AuthContext);
     const navigate = useNavigate()
-  
-          if(isAuthenticated){
-              return(
-                  <Component />
-              )
-          } else{
-            errorNotification("You must be a logged in user/business to do this!")
-            navigate("/")
-          }
-  }
+    console.log(isAuth)
+    if (isAuth) {
+        return (
+            <Component />
+        )
+    } else {
+        errorNotification("You must be a logged in user/business to do this!")
+        navigate("/")
+    }
+}
