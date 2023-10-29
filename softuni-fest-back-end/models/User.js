d6ad3ed490947fbfa2d7fc4aebf3571dc9a61bde
dingi7 <kamenkanev88@gmail.com>
2023-10-29 10:00:48 +0200

    .

--- softuni-fest-back-end/models/User.js
+++ softuni-fest-back-end/models/User.js
@@ -19,7 +19,7 @@ const userSchema = new Schema({
             // product: { type: Schema.Types.ObjectId, ref: 'Product' },
             product: {
                 name: { type: String, required: [true, 'Name is required'] },
-                description: { type: String, required: true, minlength: [10, 'Description must be at least 10 characters long'] },
+                description: { type: String, required: true },
                 price: { type: Number, required: true },
             },
             paidWith: { type: String },
